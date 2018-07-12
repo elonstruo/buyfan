d86ae6a1d6f1f4cf89f8941b2c03106eb65fa56f
elonstruo <15521399838@163.com>
2018-07-12 23:55:10 +0800

    shopcart

--- mini_buyfan/pages/menu/menu.js
+++ mini_buyfan/pages/menu/menu.js
@@ -5,10 +5,25 @@ Page({
      * 页面的初始数据
      */
     data: {
-		activeCategoryId: "0",
-		selectedId: "1",
-		type_sort: ["特价", "销量好评", "商家推荐"],
-		list: ["特价", "销量好评", "商家推荐"],
+        activeCategoryId: "0",
+        selectedId: "1",
+        type_sort: ["特价", "销量好评", "商家推荐"],
+        list: [
+            {
+                img:"../../images/shopimg.jpg",
+                name:"樱花碧根果奶缇樱花碧根果奶缇",
+                description:"营养极高的碧根果牛奶搭配淡淡香气的营养极高的碧根果牛奶搭配淡淡香气的",
+                sale:"444",
+                price:"16.60"
+            },
+            {
+                img: "../../images/shopimg.jpg",
+                name: "樱花碧根果奶缇樱花碧根果奶缇",
+                description: "营养极高的碧根果牛奶搭配淡淡香气的营养极高的碧根果牛奶搭配淡淡香气的",
+                sale: "444",
+                price: "16.60"
+            }
+        ],
 
     },
 
@@ -17,20 +32,54 @@ Page({
      */
     onLoad: function(options) {
 
-	},
-	tabClick: function (e) {
-		var that = this;
-		that.setData({
-			selectedId: e.currentTarget.dataset.id
-		});
-	},
-	categoryClick: function (e) {
-		var that = this;
-		console.log(e)
-		that.setData({
-			activeCategoryId: e.currentTarget.dataset.id
-		});
-	},
+    },
+    tabClick: function(e) {
+        var that = this;
+        that.setData({
+            selectedId: e.currentTarget.dataset.id
+        });
+    },
+    categoryClick: function(e) {
+        var that = this;
+        console.log(e)
+        that.setData({
+            activeCategoryId: e.currentTarget.dataset.id
+        });
+    },
+    /**
+     * 绑定加数量事件
+     */
+    addCount(e) {
+        const index = e.currentTarget.dataset.index;
+        let carts = this.data.carts;
+        let num = carts[index].num;
+        num = num + 1;
+        carts[index].num = num;
+        this.setData({
+            carts: carts
+        });
+        this.getTotalPrice();
+    },
+
+    /**
+     * 绑定减数量事件
+     */
+    minusCount(e) {
+        console.log(e)
+        const index = e.currentTarget.dataset.index;
+        const obj = e.currentTarget.dataset.obj;
+        let carts = this.data.carts;
+        let num = carts[index].num;
+        if (num <= 1) {
+            return false;
+        }
+        num = num - 1;
+        carts[index].num = num;
+        this.setData({
+            carts: carts
+        });
+        this.getTotalPrice();
+    },
     /**
      * 生命周期函数--监听页面初次渲染完成
      */
